408eb01dd2f9250df8f837a6b7a9b5b9ada4d6cd
prabin <prabin.drag01@gmail.com>
2018-02-09 01:05:34 +0545

    Add design for video chat

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,8 +3,39 @@ import React, { Component } from 'react';
 class Video extends Component {
     render() {
         return (
-            <div className="App">
-                <div>test</div>
+            <div className="container component-screen">
+                <div className="col-xs-12 col-lg-7">
+                    <div className='publisher-video'>
+                        <img src='https://research.google.com/youtube8m/youtube-logo-light.png'
+                            alt='publiser' width='700px' />
+                    </div>
+                </div>
+                <div className="col-xs-12 col-lg-5">
+                    <div className='row'>
+                        <div className='col-xs-5'>
+                            <div className='subscriber-video'>
+                                <img src='https://s.ytimg.com/yts/img/avatar_720-vflYJnzBZ.png' width='100px' height='100px' alt='Subscriber' />
+                            </div>
+                        </div>
+                        <div className='col-xs-7' style={{ margin: '10px 0px' }}>
+                            <div> Ram Shreatha </div>
+                            <div> Status : Online </div>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-xs-5'>
+                            <div className='subscriber-video'>
+                                <img src='https://s.ytimg.com/yts/img/avatar_720-vflYJnzBZ.png' width='100px' height='100px' alt='Subscriber' />
+                            </div>
+                        </div>
+                        <div className='col-xs-7' style={{ margin: '10px 0px' }}>
+                            <div> Ram Shreatha </div>
+                            <div> Status : Online </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-xs-12 col-lg-3 message-box">
+                </div>
             </div>
         );
     }
